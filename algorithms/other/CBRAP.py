5433cefe689397a8c593b81d5dfdea7a844764be
Lilia Izri <izri@lisn.fr>
2024-09-17 22:14:14 +0200

    Add CPRAP matmul

--- algorithms/other/CBRAP.py
+++ algorithms/other/CBRAP.py
@@ -64,13 +64,17 @@ class CBRAP(LinearRegression_CBRAP):
     """ 
         CBRAP class recommending over a finite set
     """
-    def recommend(self):
+    def recommend_loop(self):
+        """
+            Initial Implementation using for loop
+        """
         beta = self.scale * ((self.sigma * np.sqrt(self.d * np.log10((1 + self.t) / self.delta))) + np.sqrt(self.lam) + 1)
         # Compute UCB for each action
         ucb_max = float('-inf')
         a_max = None
         a_proj_max = None
         for idx, (a_proj, a) in enumerate(zip(self.action_set_proj, self.action_set)):
+            # Compute UCB for each action
             ucb = (a_proj @ self.theta_est) + (beta * np.sqrt(a_proj @ (self.Vinv @ a_proj)))
             if ucb > ucb_max:
                 ucb_max = ucb
@@ -78,3 +82,29 @@ class CBRAP(LinearRegression_CBRAP):
                 a_max = a   
                 self.selected_action_idx = idx 
         return a_proj_max, a_max
+    
+    def recommend_matmul(self):
+        """
+            Implementation using matrix multiplication with einsum 
+        """
+        beta = self.scale * ((self.sigma * np.sqrt(self.d * np.log10((1 + self.t) / self.delta))) + np.sqrt(self.lam) + 1)
+        # Compute UCB for each action
+        A_Theta = self.action_set_proj @ self.theta_est                  
+        A_Vinv_A = np.einsum('ij,jk,ik->i', self.action_set_proj, self.Vinv, self.action_set_proj)  # get diagonal of (A @ V @ A^T) Of size n 
+        sqrt_A_Vinv_A = np.sqrt(A_Vinv_A)  
+        ucb_values = A_Theta + beta * sqrt_A_Vinv_A
+
+        # find the maximum UCB and corresponding index
+        ucb_max_idx = np.argmax(ucb_values)
+        ucb_max = ucb_values[ucb_max_idx]
+
+        # Rretrieve the corresponding action and projection
+        a_proj_max = self.action_set_proj[ucb_max_idx]
+        a_max = self.action_set[ucb_max_idx]
+        self.selected_action_idx = ucb_max_idx
+        return a_proj_max, a_max
+
+    def recommend(self):
+        # a = self.recommend_matmul()
+        a = self.recommend_loop()
+        return a
