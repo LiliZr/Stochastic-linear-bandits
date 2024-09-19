8dfcd5f14df0969a35e2c8f51a721e87c55e318a
Lilia Izri <izri@lisn.fr>
2024-09-19 17:06:17 +0200

    Update LinUCB

--- algorithms/baseline/LinUCB.py
+++ algorithms/baseline/LinUCB.py
@@ -16,7 +16,7 @@ class LinUCB(LinearRegression):
     """ 
         LinUCB class recommending over a finite set or convex set
     """
-    def recommend(self):
+    def recommend_loop(self):
         beta_sqrt = ((np.sqrt(self.lam) * self.m2) + (self.sigma * np.sqrt((- 2 * np.log10(self.delta)) + 
                                (self.d * np.log10(1 + ((self.t * self.L**2)/(self.lam*self.d)))))) ) * self.scale
         ##### Finite set case
@@ -37,3 +37,28 @@ class LinUCB(LinearRegression):
         else:
             # TODO
             return None
+        
+    def recommend_matmul(self):
+        beta_sqrt = ((np.sqrt(self.lam) * self.m2) + (self.sigma * np.sqrt((- 2 * np.log10(self.delta)) + 
+                               (self.d * np.log10(1 + ((self.t * self.L**2)/(self.lam*self.d)))))) ) * self.scale
+
+        # Compute intermediate results
+        A_Theta = self.action_set @ self.theta_est
+        A_Vinv_A = np.einsum('ij,ij->i', self.action_set, self.action_set @ self.Vinv.T)  
+        sqrt_A_V_A = np.sqrt(A_Vinv_A)     
+
+        # Compute UCB values for all actions and find maximum
+        ucb_values = A_Theta + beta_sqrt * sqrt_A_V_A  
+        ucb_values = np.round(ucb_values, decimals=5)
+        ucb_max_idx = np.argmax(ucb_values)
+        ucb_max = ucb_values[ucb_max_idx]
+
+        a_max = self.action_set[ucb_max_idx]
+        self.selected_action_idx = ucb_max_idx
+  
+        return a_max
+
+    def recommend(self):
+        a = self.recommend_matmul()
+        # a = self.recommend_loop()
+        return a
