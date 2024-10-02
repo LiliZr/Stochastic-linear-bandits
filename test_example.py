412faac48cba1121516f205f3c754a9f2264f77a
Lilia Izri <izri@lisn.fr>
2024-10-02 16:36:21 +0200

    Remove comments

--- test_example.py
+++ test_example.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     # Time limit (in sec)
     time_limit = 50000
     # Max iterations
-    T = 3000 # 15000
+    T = 300 # 15000
     # Number of runs (To approximate in expectation) and seeds
     nb_runs = 1  # 15
     loop_RP = 1
@@ -81,61 +81,61 @@ if __name__ == "__main__":
             loop_RP_ = loop_RP if dataset not in ['Random'] and model_.__name__  in ['CBRAP', 'ConfidenceBall1_FJLT', 'ConfidenceBall1_FJLT_Cholesky'] else 1
             # Get hyperparameters and combination of values to test
             params_model = {}
-            # for p in params_models[model_.__name__ ]:
-            #     params_model[p] = params_values[p]
-            # for vals in product(*params_model.values()):
-            # #    try:
-            #         params = dict(zip(params_model, vals))
-            #         params_str = ''.join([f'_{key}={value}' for key, value in params.items()])
-            #         # params['loop'] = loop
-            #         #### Models to test
-            #         models = [model_,  ]
-            #         models_names = [m.__name__ for m in models]
-            #         results = {name: 
-            #                 {'reward': np.zeros((nb_runs_, T)),
-            #                     'regret': np.zeros((nb_runs_, T)),
-            #                     'time_cpu': np.zeros((nb_runs_, T)),
-            #                     'time_wall': np.zeros((nb_runs_, T)),
-            #                     'memory': np.zeros((nb_runs_, T)),}
-            #                 for name in models_names}
+            for p in params_models[model_.__name__ ]:
+                params_model[p] = params_values[p]
+            for vals in product(*params_model.values()):
+            #    try:
+                    params = dict(zip(params_model, vals))
+                    params_str = ''.join([f'_{key}={value}' for key, value in params.items()])
+                    # params['loop'] = loop
+                    #### Models to test
+                    models = [model_,  ]
+                    models_names = [m.__name__ for m in models]
+                    results = {name: 
+                            {'reward': np.zeros((nb_runs_, T)),
+                                'regret': np.zeros((nb_runs_, T)),
+                                'time_cpu': np.zeros((nb_runs_, T)),
+                                'time_wall': np.zeros((nb_runs_, T)),
+                                'memory': np.zeros((nb_runs_, T)),}
+                            for name in models_names}
 
 
-            #         for i in range(nb_runs_):
-            #             print(f'{dataset}____Run {i}/{nb_runs_}____')
-            #             print(f'{model_.__name__}___{params_str}____')
-            #             # Optimal model
-            #             optimal = Optimal(theta, action_set=action_set, seed=seeds[i])
-            #             optimal.run(T, time_limit=time_limit)
-            #             reward_max = optimal.cumulative_reward
-            #             reward_, regret_, time_cpu_, time_wall_, memory_ = [], [], [], [], []
-            #             for j in range(loop_RP_):
-            #                 if model_.__name__ not in ['ConfidenceBall1_FJLT',]:
-            #                     params['loop'] = loop
-            #                 # Other models
-            #                 for model, model_name in zip(models, models_names):
-            #                     model = model(theta, action_set=action_set, sigma=sigma, seed=seeds[i],  delta=1/T, **params)
-            #                     model.run(T, time_limit=time_limit)
-            #                     reward_.append(model.cumulative_reward)
-            #                     regret_.append(reward_max - model.cumulative_reward)
-            #                     time_cpu_.append(model.cpu_time)
-            #                     time_wall_.append(model.wall_time)
-            #                     memory_.append(model.memory_peak)
-            #             results[model_name]['reward'][i] = np.mean(reward_, axis=0)
-            #             results[model_name]['regret'][i] = np.mean(regret_, axis=0)
-            #             results[model_name]['time_cpu'][i] = np.mean(time_cpu_, axis=0)
-            #             results[model_name]['time_wall'][i] = np.mean(time_wall_, axis=0)
-            #             results[model_name]['memory'][i] = np.mean(memory_, axis=0)
+                    for i in range(nb_runs_):
+                        print(f'{dataset}____Run {i}/{nb_runs_}____')
+                        print(f'{model_.__name__}___{params_str}____')
+                        # Optimal model
+                        optimal = Optimal(theta, action_set=action_set, seed=seeds[i])
+                        optimal.run(T, time_limit=time_limit)
+                        reward_max = optimal.cumulative_reward
+                        reward_, regret_, time_cpu_, time_wall_, memory_ = [], [], [], [], []
+                        for j in range(loop_RP_):
+                            if model_.__name__ not in ['ConfidenceBall1_FJLT',]:
+                                params['loop'] = loop
+                            # Other models
+                            for model, model_name in zip(models, models_names):
+                                model = model(theta, action_set=action_set, sigma=sigma, seed=seeds[i],  delta=1/T, **params)
+                                model.run(T, time_limit=time_limit)
+                                reward_.append(model.cumulative_reward)
+                                regret_.append(reward_max - model.cumulative_reward)
+                                time_cpu_.append(model.cpu_time)
+                                time_wall_.append(model.wall_time)
+                                memory_.append(model.memory_peak)
+                        results[model_name]['reward'][i] = np.mean(reward_, axis=0)
+                        results[model_name]['regret'][i] = np.mean(regret_, axis=0)
+                        results[model_name]['time_cpu'][i] = np.mean(time_cpu_, axis=0)
+                        results[model_name]['time_wall'][i] = np.mean(time_wall_, axis=0)
+                        results[model_name]['memory'][i] = np.mean(memory_, axis=0)
                             
 
-            #         ######################## Plots and results saving ########################
-            #         params_ = {
-            #             model_.__name__:params
-            #         }
-            #         params_exp = {'d':d, 'T':T, 'nb_actions':nb_actions, 'sigma':sigma, 
-            #                     'dataset':dataset, 'nb_runs':nb_runs, 'loop_RP': loop_RP}
+                    ######################## Plots and results saving ########################
+                    params_ = {
+                        model_.__name__:params
+                    }
+                    params_exp = {'d':d, 'T':T, 'nb_actions':nb_actions, 'sigma':sigma, 
+                                'dataset':dataset, 'nb_runs':nb_runs, 'loop_RP': loop_RP}
 
-            #         plot_save_intermediate_results(results, params_exp, params_models=params_,
-            #                                                         PATH=PATH_, params_comparison=True, )
+                    plot_save_intermediate_results(results, params_exp, params_models=params_,
+                                                                    PATH=PATH_, params_comparison=True, )
         path_results = path_to_results(dataset, sub_path(d, sigma, nb_actions, T, dataset, nb_runs))
         name = 'Loop-' + dataset if loop else 'Matmul-' + dataset
         results_mem = results_memory(PATH_ + path_results, k=params_values['k'], m=params_values['m'])
